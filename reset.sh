764d4c5a8b6944cc99417ac633f38ed3a90e8815
JimCodified <jim.armstrong@docker.com>
2020-05-22 15:40:36 -0500

    reset 200522-1540

---
+++ reset.sh
@@ -0,0 +1,7 @@
+git checkout demo2start -- alpine-curl/Dockerfile
+git checkout demo2start -- alpha-blog/Dockerfile
+git checkout demo2start -- alpha-blog/Dockerfile.rubyfixes
+git checkout demo2start -- alpha-blog/Gemfile
+git checkout demo2start -- alpha-blog/Gemfile.lock
+git add -A
+git commit -m $()
